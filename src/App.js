a29e376e79d27aa91bd6391e513c905798926ef7
Arynn Boniface <arynn.boniface@gmail.com>
2021-02-02 16:55:41 -0500

    why dont i remember how to do this?

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
 import React from 'react'
+import Names from './Names'
 // import recipes from './recipes.json'
-import './App.css';
+// import './App.css';
 
-function App() {
-  const fetchFood = () => {
+class App extends React.Component {
+  fetchFood = () => {
     fetch('https://api.scraper.ai/api/website/59b18671-3e6b-4829-9136-7c90f76e3c1b?api_key=44ba2e0f7ba967126f2888c995bcb535&json')
     .then(resp => resp.json())
-    .then(data => showNames(data.data))
+    .then(data => data)
   }
 
-  const showNames = (foods) => {
-    foods.forEach(food => {
-      console.log(food.names)
-    })
+  render() {
+    return <Names foods={() => {this.fetchFood()}} />
   }
-
-  return <div> {fetchFood()}</div>
 }
 
 export default App;
