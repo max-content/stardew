f90d077fe6077397fe57df687622d86416bf74c3
Arynn Boniface <arynn.boniface@gmail.com>
2021-02-02 18:10:03 -0500

    cant get list to render?

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import React from 'react'
-import Names from './Names'
-// import recipes from './recipes.json'
+import recipes from './recipes.json'
 // import './App.css';
 
 class App extends React.Component {
-  fetchFood = () => {
-    fetch('https://api.scraper.ai/api/website/59b18671-3e6b-4829-9136-7c90f76e3c1b?api_key=44ba2e0f7ba967126f2888c995bcb535&json')
-    .then(resp => resp.json())
-    .then(data => data)
-  }
 
   render() {
-    return <Names foods={() => {this.fetchFood()}} />
+    const list = recipes.data
+
+    const renderFoods = (foods) => {
+      foods.map(food => {
+        return <li> {food.names} </li>
+      });
+    }
+
+    console.log(list[0].names);
+    return (<div>
+      <ul> {renderFoods(list)}</ul>
+    </div>
+    )
   }
+
 }
 
 export default App;
